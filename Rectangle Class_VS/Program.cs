e236e94cbc95c909acb885b369da8e0027564456
Doan Minh Duy <duydoan812@gmail.com>
2025-03-24 23:02:14 +0700

    Finish Commit

---
+++ Rectangle Class_VS/Program.cs
@@ -0,0 +1,49 @@
+﻿
+public class Rectangle
+{
+    double width;
+    double height;
+
+    public Rectangle()
+    {
+
+    }
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public double GetArea()
+    {
+        return this.width * this.height;
+    }
+    public double GetPerimeter()
+    {
+        return (this.width + this.height)*2;
+    }
+    public string Display()
+    {
+        return "Rectangle{" + "width = " + width + ", height = " + height + "}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter width: ");
+        double width = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(width, height);
+
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
+        Console.WriteLine("Rectangle Perimeter : " + rectangle.GetPerimeter());
+        Console.WriteLine("Display: " + rectangle.Display());
+    }
+    
+}
+
+
+
